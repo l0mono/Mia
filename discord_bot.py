72bbf7c74a001098ea53a20e8bf1ae8abe0104f3
l0mono <matsudayuki030321@gmail.com>
2022-09-08 10:33:18 +0900

    first commit

---
+++ discord_bot.py
@@ -0,0 +1,48 @@
+import discord
+import MeCab
+import yaml
+from datetime import datetime
+
+with open('input.yaml') as file:
+    object = yaml.safe_load(file)
+    TOKEN = object['TOKEN']
+    CHANNELID = object['CHANNELID']
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+@client.event
+async def on_message(message):
+    if not message.author.bot:
+        if message.content == "in":
+            moment = datetime.now().strftime('%H:%M')
+            global in_time
+            in_time = int(moment[0:2])
+            channel = client.get_channel(CHANNELID)
+            await channel.send("Have fun at work! The current time is {}. When you leave work, please say 'out' on this channel".format(moment))
+
+        elif message.content == "out":
+            moment = datetime.now().strftime('%H:%M')
+            global out_time
+            out_time = int(moment[0:2])
+            working = out_time-in_time
+            channel = client.get_channel(CHANNELID)
+            wage = 1000
+            await channel.send("Take a good rest!! The current time is {}.".format(moment))
+            await channel.send("Today's work time is {} hour.".format(working))
+            await channel.send("Today's wage is {} yen".format(working*wage))
+
+client.run(TOKEN)
+
+
+
+
+
+client.run(TOKEN)
